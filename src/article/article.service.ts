a9236d03d5d075fa9f4186158e083d4aa1f97b2a
SpectreAlan <comjszoo@gmail.com>
2023-12-23 00:11:44 +0700

    add some entities

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -3,7 +3,7 @@ import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Article } from '../schemas/article.schema';
+import { Article } from './entities/article.entity';
 
 @Injectable()
 export class ArticleService {
--- src/article/entities/article.entity.ts
+++ src/article/entities/article.entity.ts
@@ -1,1 +1,45 @@
-export class Article {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type ArticleDocumentType = HydratedDocument<Article>;
+
+@Schema()
+export class Article {
+  @Prop({ required: true, unique: true })
+  title: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: true })
+  content: string;
+
+  @Prop({ required: true })
+  category: string;
+
+  @Prop({ required: true })
+  tag: string;
+
+  @Prop({ required: true })
+  cover: string;
+
+  @Prop({ required: true })
+  keywords: string;
+
+  @Prop({ default: 0 })
+  scan: number;
+
+  @Prop({ default: true })
+  catalogue: boolean;
+
+  @Prop({ default: 1 })
+  status: number;
+
+  @Prop({ default: '' })
+  date_create: string;
+
+  @Prop({ default: '' })
+  date_update: string;
+}
+
+export const ArticleSchema = SchemaFactory.createForClass(Article);
