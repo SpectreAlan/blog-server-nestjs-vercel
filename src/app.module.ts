a46675cc27d5d6e67f3d3527252cd9e6544172ba
SpectreAlan <comjszoo@gmail.com>
2023-12-22 21:30:21 +0700

    add mongodb config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as process from 'process';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.DB_HOST)],
   controllers: [AppController],
   providers: [AppService],
 })
