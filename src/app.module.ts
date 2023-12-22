056255b7989ee48a1dab79dc8dc8373413b96f48
SpectreAlan <comjszoo@gmail.com>
2023-12-22 23:05:30 +0700

    edit mongodb config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import * as process from 'process';
+import { ArticleModule } from './article/article.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.DB_HOST)],
+  imports: [
+    MongooseModule.forRoot(process.env.DB_CONNECTION_URL, {}),
+    ArticleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
