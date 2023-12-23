74d192a1aec8b5849dd02eb920e766f23b2a3d2a
SpectreAlan <comjszoo@gmail.com>
2023-12-23 23:59:39 +0700

    add validationPipe

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,18 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, HttpStatus } from '@nestjs/common';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Article } from './entities/article.entity';
+import { ArticleEntity } from './entities/article.entity';
 
 @Injectable()
 export class ArticleService {
   constructor(
-    @InjectModel('Article') private readonly articleModel: Model<Article>,
+    @InjectModel('Article')
+    private readonly articleEntity: Model<ArticleEntity>,
   ) {}
-  async create(createArticleDto: CreateArticleDto): Promise<Article> {
-    const article = await this.articleModel.create(createArticleDto);
-    return article.save();
+
+  async create(user, article: CreateArticleDto): Promise<ArticleEntity> {
+    const { title } = article;
+    const doc = await this.articleEntity.findOne({ title });
+    if (doc) {
+      throw new HttpException('文章已存在', HttpStatus.BAD_REQUEST);
+    }
+    const create = await this.articleEntity.create(article);
+    return create.save();
   }
 
   findAll() {
