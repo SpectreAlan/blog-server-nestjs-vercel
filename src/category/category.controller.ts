ded4b37c6c8ea80e8c375c9a92bc4c36a898be01
SpectreAlan <comjszoo@gmail.com>
2024-01-04 21:09:28 +0800

    update category module

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -6,40 +6,53 @@ import {
   Patch,
   Param,
   Delete,
+  UsePipes,
+  Query,
 } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { ClassValidatorPipe } from '../core/pipes/validationPipe';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Post()
+  @UsePipes(ClassValidatorPipe)
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoryService.create(createCategoryDto);
   }
 
   @Get()
-  findAll() {
-    return this.categoryService.findAll();
+  findAll(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+    @Query('title') title: string,
+  ) {
+    return this.categoryService.findAll({
+      page,
+      limit,
+      title,
+    });
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.categoryService.findOne(+id);
+    return this.categoryService.findOne(id);
   }
 
   @Patch(':id')
+  @UsePipes(ClassValidatorPipe)
   update(
     @Param('id') id: string,
     @Body() updateCategoryDto: UpdateCategoryDto,
   ) {
-    return this.categoryService.update(+id, updateCategoryDto);
+    return this.categoryService.update(id, updateCategoryDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.categoryService.remove(+id);
+    return this.categoryService.remove(id);
   }
 }
--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,14 +1,27 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CategorySchema } from './entities/category.entity';
+import { ValidateIdMiddleware } from '../core/middleware/validate-id.middleware';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Category', schema: CategorySchema }]),
   ],
+  exports: [CategoryService],
   controllers: [CategoryController],
   providers: [CategoryService],
 })
-export class CategoryModule {}
+export class CategoryModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ValidateIdMiddleware)
+      .forRoutes({ path: 'category/:id', method: RequestMethod.ALL });
+  }
+}
