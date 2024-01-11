477e02e248593c228eef59c9cd368f8e5ac18665
SpectreAlan <comjszoo@gmail.com>
2024-01-11 12:57:50 +0800

    add common utils

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,3 +1,6 @@
+import { Response as ExpressResponse } from 'express';
+import { sign } from 'jsonwebtoken';
+
 export const getAliOSSConfig = () => {
   return {
     region: process.env.NEXT_PUBLIC_OSS_ALIYUN_REGION,
@@ -5,4 +8,28 @@ export const getAliOSSConfig = () => {
     accessKeySecret: process.env.NEXT_PUBLIC_OSS_ALIYUN_SECRET,
     bucket: process.env.NEXT_PUBLIC_OSS_ALIYUN_BUCKET,
   };
+};
+
+export const responseLoginResult = (res: ExpressResponse, user) => {
+  const { status, nickName, role, avatar, account, _id: id, password } = user;
+  let encodedUser: string = 'null';
+  if (status) {
+    encodedUser = Buffer.from(
+      JSON.stringify({
+        nickName,
+        role,
+        avatar,
+        status,
+        id,
+        hasPassword: !!password,
+      }),
+    ).toString('base64');
+    const token = sign({ account, role }, process.env.SECRET_KEY, {
+      expiresIn: '1h',
+    });
+    res.cookie('token', token, { httpOnly: true });
+  }
+  return res.redirect(
+    'http://localhost:3000/home.html?token=' + encodeURIComponent(encodedUser),
+  );
 };
