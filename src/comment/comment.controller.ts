a83ca21b8f2d57b31dff58e44c0a93a3cee55a2b
SpectreAlan <comjszoo@gmail.com>
2024-01-12 14:46:01 +0800

    add comment module

---
+++ src/comment/comment.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Query,
+  UsePipes,
+  UseInterceptors,
+} from '@nestjs/common';
+import { CommentService } from './comment.service';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { ClassValidatorPipe } from '../core/pipes/validationPipe';
+import { ResponseInterceptor } from '../core/interceptors/response.interceptor';
+
+@Controller('comment')
+@UseInterceptors(ResponseInterceptor)
+export class CommentController {
+  constructor(private readonly commentService: CommentService) {}
+
+  @Post()
+  @UsePipes(ClassValidatorPipe)
+  create(@Body() createCommentDto: CreateCommentDto) {
+    return this.commentService.create(createCommentDto);
+  }
+
+  @Get()
+  findAll(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+    @Query('title') title: string,
+  ) {
+    return this.commentService.findAll({ page, limit, title });
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.commentService.remove(id);
+  }
+}
---
+++ src/comment/comment.module.ts
@@ -0,0 +1,31 @@
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { CommentService } from './comment.service';
+import { CommentController } from './comment.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CommentSchema } from './entities/comment.entity';
+import { ValidateIdMiddleware } from '../core/middleware/validate-id.middleware';
+import { AuthMiddleware } from '../core/middleware/auth.middleware';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Comment', schema: CommentSchema }]),
+  ],
+  exports: [CommentService],
+  controllers: [CommentController],
+  providers: [CommentService],
+})
+export class CommentModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ValidateIdMiddleware)
+      .forRoutes({ path: 'comment/:id', method: RequestMethod.ALL });
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes({ path: 'comment', method: RequestMethod.ALL });
+  }
+}
