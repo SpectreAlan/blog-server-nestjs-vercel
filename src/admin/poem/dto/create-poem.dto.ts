9abfd9417ae0d007c5c1372be5a716fc88b84f08
SpectreAlan <comjszoo@gmail.com>
2024-01-13 18:40:49 +0800

    add schedule

--- src/admin/poem/dto/create-poem.dto.ts
+++ src/admin/poem/dto/create-poem.dto.ts
@@ -2,4 +2,6 @@ import { IsNotEmpty } from 'class-validator';
 export class CreatePoemDto {
   @IsNotEmpty({ message: '内容必填' })
   readonly content: string;
+  readonly type: string;
+  readonly author: string;
 }
