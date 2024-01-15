23157df6343e30fd1e1a1db881d223929ebe9ef6
SpectreAlan <comjszoo@gmail.com>
2024-01-15 20:53:01 +0800

    edit dto

--- src/admin/comment/dto/create-comment.dto.ts
+++ src/admin/comment/dto/create-comment.dto.ts
@@ -1,5 +1,14 @@
 import { IsNotEmpty } from 'class-validator';
 export class CreateCommentDto {
-  @IsNotEmpty({ message: '标签名称必填' })
-  readonly title: string;
+  @IsNotEmpty({ message: '内容必填' })
+  readonly content: string;
+
+  @IsNotEmpty({ message: '文章ID必填' })
+  readonly article: string;
+
+  @IsNotEmpty({ message: '昵称必填' })
+  readonly nickName: string;
+
+  readonly email: string;
+  readonly parentId: string;
 }
