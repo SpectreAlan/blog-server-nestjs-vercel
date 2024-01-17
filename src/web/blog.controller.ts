26b94773dc1dd4ca536d3d77c5966d5babb10c23
SpectreAlan <comjszoo@gmail.com>
2024-01-17 12:06:14 +0800

    add poem service

--- src/web/blog.controller.ts
+++ src/web/blog.controller.ts
@@ -11,6 +11,8 @@ import {
 import { ArticleService } from '../admin/article/article.service';
 import { CommentService } from '../admin/comment/comment.service';
 import { TagService } from '../admin/tag/tag.service';
+import { PoemService } from '../admin/poem/poem.service';
+import { SettingService } from '../admin/setting/setting.service';
 import { CreateCommentDto } from '../admin/comment/dto/create-comment.dto';
 import { ClassValidatorPipe } from '../core/pipes/validationPipe';
 import { ResponseInterceptor } from '../core/interceptors/response.interceptor';
@@ -22,6 +24,8 @@ export class BlogController {
     private readonly articleService: ArticleService,
     private readonly commentService: CommentService,
     private readonly tagService: TagService,
+    private readonly poemService: PoemService,
+    private readonly settingService: SettingService,
   ) {}
 
   @Post('comment')
@@ -49,6 +53,16 @@ export class BlogController {
   @Get('tags')
   async tags() {
     return this.tagService.findAll({ page: 1, limit: 100, title: '' });
+  }
+
+  @Get('poem')
+  async poem() {
+    return this.poemService.getRandomPoems();
+  }
+
+  @Get('visitor')
+  async visitor() {
+    return this.settingService.incrementVisitorCount();
   }
 
   @Get('list')
--- src/web/blog.module.ts
+++ src/web/blog.module.ts
@@ -4,10 +4,19 @@ import { BlogService } from './blog.service';
 import { TagModule } from '../admin/tag/tag.module';
 import { CategoryModule } from '../admin/category/category.module';
 import { CommentModule } from '../admin/comment/comment.module';
+import { PoemModule } from '../admin/poem/poem.module';
+import { SettingModule } from '../admin/setting/setting.module';
 import { ArticleModule } from '../admin/article/article.module';
 
 @Module({
-  imports: [TagModule, CategoryModule, CommentModule, ArticleModule],
+  imports: [
+    TagModule,
+    CategoryModule,
+    CommentModule,
+    ArticleModule,
+    PoemModule,
+    SettingModule,
+  ],
   controllers: [BlogController],
   providers: [BlogService],
 })
