8856c5505dc89cdbe9d4caa56738cb0cb83ee6cc
SpectreAlan <comjszoo@gmail.com>
2024-01-27 13:29:27 +0800

    ADD session

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
   app.useStaticAssets('public');
+  app.use(
+    session({
+      secret: 'blog-server-secret',
+      rolling: true,
+      name: 'blog-server',
+      cookie: { maxAge: null },
+    }),
+  );
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
