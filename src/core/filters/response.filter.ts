89740339cb634b08644470019d6eae2cc5cf4314
SpectreAlan <comjszoo@gmail.com>
2024-02-08 20:31:14 +0800

    Record error log

--- src/core/filters/response.filter.ts
+++ src/core/filters/response.filter.ts
@@ -1,13 +1,30 @@
 import { Catch, ArgumentsHost } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
 import * as CryptoJS from 'crypto-js';
+import { LogService } from '../../admin/log/log.service';
 
 @Catch()
 export class ResponseFilter extends BaseExceptionFilter {
+  constructor(private readonly logService: LogService) {
+    super();
+  }
+
   catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
+    const { body, query, method, headers, connection, url } = ctx.getRequest();
     const { status = 400, message = 'Internal Server Error' } = exception;
+    this.logService.create({
+      ip: headers['x-forwarded-for'] || connection.remoteAddress,
+      userAgent: headers['user-agent'],
+      method,
+      body: JSON.stringify(body),
+      query: JSON.stringify(query),
+      url,
+      status,
+      message,
+    });
+
     const encrypted = CryptoJS.AES.encrypt(
       JSON.stringify({
         code: status,
