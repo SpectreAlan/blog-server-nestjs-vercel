4ab87ce0af27a0b9176218ab9e155d4a3fe62f63
SpectreAlan <comjszoo@gmail.com>
2024-02-08 20:32:07 +0800

    Add parameter verification

--- src/admin/user/dto/login-sms.dto.ts
+++ src/admin/user/dto/login-sms.dto.ts
@@ -6,3 +6,8 @@ export class LoginSmsDto {
   @IsNotEmpty({ message: '验证码必填' })
   readonly sms: string;
 }
+
+export class LoginGetSmsDto {
+  @IsNotEmpty({ message: '手机号必填' })
+  readonly phone: number;
+}
