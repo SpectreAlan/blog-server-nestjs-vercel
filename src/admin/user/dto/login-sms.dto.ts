cebb008cb5683a7a1845914a7400e14d0fd844df
SpectreAlan <comjszoo@gmail.com>
2024-02-08 13:24:25 +0800

    add login dto of sms

--- src/admin/user/dto/login-sms.dto.ts
+++ src/admin/user/dto/login-sms.dto.ts
@@ -3,6 +3,6 @@ export class LoginSmsDto {
   @IsNotEmpty({ message: '手机号必填' })
   readonly phone: string;
 
-  @IsNotEmpty({ message: '密码必填' })
+  @IsNotEmpty({ message: '验证码必填' })
   readonly sms: string;
 }
